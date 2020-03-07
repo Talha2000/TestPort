c43799b28348d82821846d9e953af84d425ce0da
Talha2000 <52087016+Talha2000@users.noreply.github.com>
2020-03-07 14:22:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+//const scroll = new SmoothScroll('.navbar a[href*="#"]', {
+// speed: 500
+//}); 
+/* Set the width of the side navigation to 0 */
+
+
+/* This is for the Python side bar */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+  }
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+  /* This is for the Bash side bar */
+  function openNav2() {
+    document.getElementById("mySidenav2").style.width = "100%";
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+  }
+  
+  /* This is for the Haskell side bar */
+  function openNav3() {
+    document.getElementById("mySidenav3").style.width = "100%";
+  }
+  function closeNav3() {
+    document.getElementById("mySidenav3").style.width = "0";
+  }
+  
+  /* This is for the Java side bar */
+  function openNav4() {
+    document.getElementById("mySidenav4").style.width = "100%";
+  }
+  function closeNav4() {
+    document.getElementById("mySidenav4").style.width = "0";
+  }
+
+  function openNav5() {
+    document.getElementById("mySidenav5").style.width = "100%";
+  }
+  function closeNav5() {
+    document.getElementById("mySidenav5").style.width = "0";
+  }
