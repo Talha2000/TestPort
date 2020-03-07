0cde7295704590bebf75a6f24afc30722517c220
Talha2000 <52087016+Talha2000@users.noreply.github.com>
2020-03-07 18:55:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -43,3 +43,89 @@ function openNav() {
   function closeNav5() {
     document.getElementById("mySidenav5").style.width = "0";
   }
+
+  function openNav6() {
+    document.getElementById("mySidenav6").style.width = "100%";
+  }
+  function closeNav6() {
+    document.getElementById("mySidenav6").style.width = "0";
+  }
+
+
+// ES6 Class
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 4);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 100;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init2);
+
+
+// Init App
+
+function init2() {
+  const txtElement = document.querySelector('.txt-type2');
+
+  const words = JSON.parse(txtElement.getAttribute('data-words2'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+// Dark, light mode changes
+
+function myFunction()  {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
